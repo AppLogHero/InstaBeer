3aa0b72c9abb76c1f0d2dcbaabcc2cd20e5a47e8
AppLog <julien.delferiere@gmail.com>
2021-04-24 16:28:29 +0200

    first commit

---
+++ instabeer/instabeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  instabeerApp.swift
+//  instabeer
+//
+//  Created by Julien Delferiere on 24/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct instabeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
